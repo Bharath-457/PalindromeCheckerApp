6a0816260cf2c18a7c50d65eb7dd6c912bf7d95b
Bharath-457 <sr4783@srmist.edu.in>
2026-02-24 15:51:50 +0530

    UC2 Done

--- APP/src/PalindromeCheckerApp.java
+++ APP/src/PalindromeCheckerApp.java
@@ -2,8 +2,25 @@ public class PalindromeCheckerApp {
 
     public static void main(String[] args) {
 
-        System.out.println("Welcome to Palindrome Checker App Management System");
-        System.out.println("Version: 1.0");
-        System.out.println("System initialized successfully.");
+        System.out.println("=== Palindrome Checker App ===");
+
+        // Step 1: Hardcoded string
+        String text = "madam";
+
+        // Step 2: Reverse the string
+        String reversed = "";
+
+        for (int i = text.length() - 1; i >= 0; i--) {
+            reversed += text.charAt(i);
+        }
+
+        // Step 3: Check palindrome
+        if (text.equals(reversed)) {
+            System.out.println(text + " is a Palindrome.");
+        } else {
+            System.out.println(text + " is NOT a Palindrome.");
+        }
+
+        System.out.println("Program Ended.");
     }
 }
